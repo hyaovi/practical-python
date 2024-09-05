721c24100b828c6a871f2cca0b48c16cce5596f9
Yaovi Houngue <hfyaovi@gmail.com>
2024-09-05 09:00:06 -0600

    Exercise 1.5 Work/bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+total_bounces = 10
+current_height = 100  # meters
+bounce = 1
+while bounce <= total_bounces:
+    current_height *= 3 / 5
+    print(bounce, current_height)
+    bounce += 1
