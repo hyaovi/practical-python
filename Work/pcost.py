e9683504718bc8999a36dc980bb47b90263ff26d
Yaovi Houngue <hfyaovi@gmail.com>
2024-09-23 23:20:35 -0600

    Excerice 1.27, pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+cost = 0
+with open("Data/portfolio.csv", "rt") as f:
+    header = next(f)
+    for line in f:
+        row = line.split(",")
+        share = int(row[1])
+        price = float(row[2])
+        cost = cost + price * share
+
+print(f"Total cost {round(cost,2)}")
